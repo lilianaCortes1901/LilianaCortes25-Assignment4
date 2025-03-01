45e05464997b3787cf8c75de6e5f8cb7e3295fad
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-02-28 20:04:38 -0600

    Creating Stack for input
    Adding scanner input to the stack & finding conditions

--- src/BalancedBrackets.java
+++ src/BalancedBrackets.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
 
 public class BalancedBrackets {
-
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter bracket sequence: ");
+
+        String[] s = {input.nextLine()};
+        Stack<String> balanceBracket = new Stack<>();
+
+        //add input string to stack
+        for(int i = 0; i < s.length; i++){
+            balanceBracket.push(s[i]);
+        }
+
+        //check if balanced
+        for(int i = 0; i < balanceBracket.size(); i++){
+            if(balanceBracket.contains("{") || balanceBracket.contains("[") || balanceBracket.contains("(")){
+                balanceBracket.pop();
+            } else if(balanceBracket.contains("]")){}
+        }
+
+        //System.out.println(balanceBracket);
     }
 }
