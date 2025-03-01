e83ab727f4bf1ef4c00e2a0e9efe4f4b9ea80e3d
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-02-28 22:53:39 -0600

    Updated DecodeString.java file
    Added conditions and updated two stacks to keep track of numbers k and the element

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -1,9 +1,40 @@
+import java.util.Stack;
 
 public class DecodeString {
+    public String toDecode(String s){
+        Stack<Character> decodeSequence = new Stack<>();
+        Stack<Integer> codeNum = new Stack<>();
+        int stringCounter = 0;
+        char valueString;
 
+        //push string s into two different stacks
+        for(int i = 0; i < s.length(); i++){
+            char codeString = s.charAt(i);
+            //checks if the first element in string is a number
+            if(codeString == '1' || codeString == '2' ||
+            codeString == '3' || codeString == '4' ||
+            codeString == '5' || codeString == '6' ||
+            codeString =='7' || codeString == '8' ||
+            codeString == '9'){
+                stringCounter = stringCounter*10 + codeString - '0';
+            } //moves next element to string stack assuming it isn't a number
+            else if(codeString == '['){
+                //number stops appearing after the first bracket
+                decodeSequence.push(codeString);
+                codeNum.push(stringCounter);
+            } else if(codeString == ']') {
+                valueString = decodeSequence.pop();
+                int count = codeNum.pop();
+                if(count > 0){
+                    decodeSequence.push(codeString);
+                }
+            }
+        }
+        return s;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
     }
 
 }
