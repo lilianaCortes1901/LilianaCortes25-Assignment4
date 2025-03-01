817f6fd17fa0b094f9dbceb1e709cb916f378175
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-02-28 23:18:05 -0600

    Updating java file
    Attempting to fix the bug

--- src/DecodeString.java
+++ src/DecodeString.java
@@ -2,31 +2,32 @@ import java.util.Stack;
 
 public class DecodeString {
     public String toDecode(String s){
-        Stack<Character> decodeSequence = new Stack<>();
+        Stack<String> decodeSequence = new Stack<>();
         Stack<Integer> codeNum = new Stack<>();
+        StringBuilder newString = new StringBuilder();
         int stringCounter = 0;
-        char valueString;
+        //char valueString;
 
         //push string s into two different stacks
         for(int i = 0; i < s.length(); i++){
             char codeString = s.charAt(i);
             //checks if the first element in string is a number
-            if(codeString == '1' || codeString == '2' ||
-            codeString == '3' || codeString == '4' ||
-            codeString == '5' || codeString == '6' ||
-            codeString =='7' || codeString == '8' ||
-            codeString == '9'){
+            if(Character.isDigit(codeString)){
                 stringCounter = stringCounter*10 + codeString - '0';
             } //moves next element to string stack assuming it isn't a number
             else if(codeString == '['){
                 //number stops appearing after the first bracket
-                decodeSequence.push(codeString);
                 codeNum.push(stringCounter);
+                decodeSequence.push(String.valueOf(codeString));
+                stringCounter = 0;
+                newString.setLength(0);
             } else if(codeString == ']') {
-                valueString = decodeSequence.pop();
+                StringBuilder temp = new StringBuilder(decodeSequence.pop());
                 int count = codeNum.pop();
                 if(count > 0){
-                    decodeSequence.push(codeString);
+                    decodeSequence.push(String.valueOf(codeString));
+                } else {
+                    decodeSequence.push();
                 }
             }
         }
@@ -34,7 +35,9 @@ public class DecodeString {
     }
 
     public static void main(String[] args) {
-
+        DecodeString decoder = new DecodeString();
+        String s = "3[a]2[bc]";
+        System.out.println(decoder.toDecode(s));
     }
 
 }
