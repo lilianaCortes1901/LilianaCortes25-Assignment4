ef09a2b605d51c8f9a4c1feaed997f34de1c4ed9
Liliana Cortes <82052771+lilianaCortes1901@users.noreply.github.com>
2025-02-28 21:12:10 -0600

    Updated BalancedBrackets.java
    Finished updating the file with finalized conditionals

--- src/BalancedBrackets.java
+++ src/BalancedBrackets.java
@@ -3,33 +3,34 @@ import java.util.Stack;
 
 public class BalancedBrackets {
     public static String isBalanced(String s){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter bracket sequence: ");
+        Stack<Character> balanceBracket = new Stack<>();
 
-        s = input.nextLine();
-        Stack<String> balanceBracket = new Stack<>();
-
-        //adding s charAt to stack
+        //pushing opening brackets to stack
         for(int i = 0; i < s.length(); i++){
             char bracket = s.charAt(i);
             if(bracket == '{' || bracket == '[' || bracket == '('){
-                balanceBracket.push(String.valueOf(bracket));
+                balanceBracket.push(bracket);
             } else if(bracket == '}' || bracket == ']' || bracket == ')'){
-                //if stack empty, not balanced
+                //if stack empty, not balanced since there is nothing to compare brackets to already in the stack
                 if(balanceBracket.isEmpty()){
                     return "No";
                 }
             }
-            if(balanceBracket.isEmpty()){
-                return "Yes";
+            //checks if brackets are paired, by comparing the char that has been popped from the stack
+            //to current remaining bracket not yet added to stack
+            char bracketTop = balanceBracket.pop();
+            if((bracket == '}' && bracketTop != '{') ||
+                    (bracket == ']' && bracketTop != '[') ||
+                    (bracket == ')' && bracketTop != '(')){
+                return "No";
             }
         }
-        return "Yes";
+        //if stack is empty, considered balanced
+        return balanceBracket.isEmpty() ? "Yes" : "No";
     }
 
     public static void main(String[] args) {
-        
-
-        //System.out.println(balanceBracket);
+        String s = "{[()]}";
+        System.out.println(isBalanced(s));
     }
 }
